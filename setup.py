678e5ea06a8e83b26832ffb3e0faf9cb8b4edf74
andosa <ando@set.ee>
2015-08-03 11:45:14 +0300

    Minor setup changes

--- setup.py
+++ setup.py
@@ -41,16 +41,9 @@ setup(
     zip_safe=False,
     keywords='treeinterpreter',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
